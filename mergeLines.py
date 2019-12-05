5ded8f73e284b15796fa7a30c0549a3f6b50ce7e
Liam Warfield <liam.warfield@gmail.com>
2019-12-04 21:43:30 -0700

    WIP: merge lines

--- mergeLines.py
+++ mergeLines.py
@@ -1,9 +1,20 @@
 import matplotlib.pyplot as plt
 
+import numpy as np
+
 def merge_lines(rholines, num):
-    rho = rholines[:num, :, 0]
-    theta = rholines[:num, :, 1]
-    print(num, rho)
-    plt.scatter(rho, theta, alpha=0.5)
-    plt.show()
+    lines = []
+    for i in range(num):
+        lines.append((rholines[i,0,0], rholines[i,0,1]))
+    
+    
+    dtype = [('rho', float), ('theta', float)]
+    lines = np.array(lines, dtype=dtype)
+    #print(lines)
+    lines = np.sort(lines, order=['theta'])
+    print(lines)
+    print(np.average(lines, axis=0))
+    print(lines.dtype)
+    #plt.scatter(lines[:,0], lines[:,1], alpha=0.5)
+    #plt.show()
     return merged_lines
